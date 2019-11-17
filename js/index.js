ceaba0137291bd814624b0b35c905ad04fc1f6c2
ITkah <ilatkacenko132@gmail.com>
2019-11-17 16:56:02 +0200

    validate

--- js/index.js
+++ js/index.js
@@ -4,19 +4,35 @@ new Vue({
         searchGif: '',
         gifs: [],
         isLoading: true,
+        isActive: false,
+        searchGif: null,
+        errors: [],
     },
     methods: {
         handleSearch() {
-            this.gifs = [];
-            this.isLoading = true;
-            fetch(`http://api.giphy.com/v1/gifs/search?q=${this.searchGif}&api_key=8yhIlmLJSr1eMYWfJxJ0sYRHKrSwapCe`)
-            .then((res) => {
-                return res.json()
-            })
-            .then((res) => {
-                this.gifs = res.data;
-                this.isLoading = false;
-            })
+            this.errors = [];
+            if (this.searchGif < 1) {
+                this.errors.push('Вы ничего не ввели');
+            } else {
+
+                this.gifs = [];
+                this.isLoading = true;
+
+                fetch(`http://api.giphy.com/v1/gifs/search?q=${this.searchGif}&api_key=8yhIlmLJSr1eMYWfJxJ0sYRHKrSwapCe`)
+                .then((res) => {
+                    return res.json()
+                })
+                .then((res) => {
+                    this.gifs = res.data;
+                    this.isLoading = false;
+                })
+            }
+        },
+        onFocus() {
+           this.isActive = true; 
+        },
+        onBlur() {
+            this.isActive = false; 
         }
     },
     created() {
